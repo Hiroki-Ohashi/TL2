635bf3c73a867ca6d21a2c9b5c7514f4bfddbe99
洋輝 大橋 <hiro0084169@outlook.jp>
2024-10-07 14:09:40 +0900

    02_03

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-	printf("Hello,World!\n");
+int main(int argc, char* argv[]) {
+	//printf("Hello,World!\n");
+
+	for (int i = 0; i < argc; i++) {
+		printf(argv[i]);
+		printf("\n");
+	}
+
 	system("pause");
 	return 0;
 }
